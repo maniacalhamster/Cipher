efe71cb2d2b112c0fd5baeed571d7c1c392ee491
Akar Singh <aks002@ucsd.edu>
2020-03-15 21:42:08 -0700

    Added decrypt function to affine.sh as well as UI

--- affine.sh
+++ affine.sh
@@ -11,23 +11,117 @@ ord() {
   LC_CTYPE=C printf '%d' "'$1"
 }
 
-msg=$1
-a=$2
-b=$3
-k=0
+function invMod() {
+	r1=$1
+	r2=$2
 
-result=""
-while [ $k -ne ${#1} ]
-do
-	char=${1:k:1}
-	val=$((`ord $char`-65))
-	dec=`chr $((($val*$a+$b) % 26 + 65))`
-	if [ "$char" == " " ]
-	then
-		result=$result" "
-	else
-		result=$result"$dec"
-	fi
+	p1=0
+	p2=1
+	q1=1
+	q2=0
+	k=-2
+
 	k=$(($k+1))
-done
-printf "$result\n"
+
+	while [ $r2 -ne 0 ]
+	do
+		k=$(($k+1))
+		a=$(($r1/$r2))
+
+		temp=$r2
+		r2=$(($r1%$r2))
+		r1=$temp
+
+		temp=$p2
+		p2=$(($a*$p2+$p1))
+		p1=$temp
+
+		temp=$q2
+		q2=$(($a*$q2+$q1))
+		q1=$temp
+	done
+
+	if [ $r1 -eq 1 ]
+	then
+		res=$(( (-1)^k * $p1))
+		while [ $res -lt 0 ]
+		do
+			res=$(($res+$2))
+		done
+		printf $res
+	else
+		return 1
+	fi
+}
+
+function decrypt() {
+	msg=$1
+	a=$2
+	b=$3
+	k=0
+	p=26
+	inv=`invMod $a $p`
+
+	result=""
+	while [ $k -ne ${#1} ]
+	do
+		char=${1:k:1}
+		val=$((`ord $char`-65))
+		cal=$(( ($val-$b)*$inv % $p))
+		while [ $cal -lt 0 ]
+		do
+			cal=$(($cal+$p))
+		done
+		dec=`chr $(($cal+65))`
+		if [ "$char" == " " ]
+			then
+				result=$result" "
+		else
+			result=$result"$dec"
+		fi
+		k=$(($k+1))
+	done
+	printf "$result\n"
+}
+
+function encrypt(){
+	msg=$1
+	a=$2
+	b=$3
+	k=0
+	p=26
+
+	result=""
+	while [ $k -ne ${#1} ]
+	do
+		char=${1:k:1}
+		val=$((`ord $char`-65))
+		dec=`chr $((($val*$a+$b) % $p + 65))`
+		if [ "$char" == " " ]
+			then
+				result=$result" "
+		else
+			result=$result"$dec"
+		fi
+		k=$(($k+1))
+	done
+	printf "$result\n"
+}
+
+function main() {
+	read -p "(e)ncrypt or (d)ecrypt? " resp
+
+	read -p "Message (CAPS): " msg
+	read -p "A value: " a
+	read -p "B value: " b
+
+	if [ "$resp" == "e" ]
+	then
+		encrypt "$msg" $a $b
+	elif [ "$resp" == "d" ]
+	then
+		decrypt "$msg" $a $b
+	fi 
+}
+
+main
